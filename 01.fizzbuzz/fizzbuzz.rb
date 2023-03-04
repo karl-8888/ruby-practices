15b10e4f154aba524b09504a586b00fc9e7f6a41
karl-8888 <n97832l60adl5@gmail.com>
2023-03-05 06:12:25 +0900

    インデント幅を4->2に変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,31 +1,28 @@
-
-# consolに1~20の数字を順番に出力する
-# numが3の場合Fizzを出力
-# numが5の場合Buzzを出力
+# 01.fizzbuzz
 
 num = 0
 
 20.times do
-    num += 1
+  num += 1
 
-    numCheck = num
-    if (numCheck %= 3) == 0
-        isMultipleOf3 = true
-    end
+  numCheck = num
+  if (numCheck %= 3) == 0
+    isMultipleOf3 = true
+  end
 
-    numCheck = num
-    if (numCheck %= 5) == 0
-        isMultipleOf5 = true
-    end
+  numCheck = num
+  if (numCheck %= 5) == 0
+    isMultipleOf5 = true
+  end
 
-    case
-    when isMultipleOf3 && isMultipleOf5
-        puts "FizzBuzz"
-    when isMultipleOf3
-        puts "Fizz"
-    when isMultipleOf5
-        puts "Buzz"
-    else
-        puts num
-    end
+  case
+  when isMultipleOf3 && isMultipleOf5
+    puts "FizzBuzz"
+  when isMultipleOf3
+    puts "Fizz"
+  when isMultipleOf5
+    puts "Buzz"
+  else
+    puts num
+  end
 end
