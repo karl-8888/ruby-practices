3a1efdad622d31d6179498454fcb08dc90b14bbf
karl-8888 <n97832l60adl5@gmail.com>
2023-03-04 16:28:55 +0900

    num値の倍数判定を追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -7,10 +7,21 @@ num = 0
 
 20.times do
     num += 1
-    case num
-    when 3
+
+    numCheck = num
+    if (numCheck %= 3) == 0
+        isMultipleOf3 = true
+    end
+    
+    numCheck = num
+    if (numCheck %= 5) == 0
+        isMultipleOf5 = true
+    end
+
+    case
+    when isMultipleOf3
         puts "Fizz"
-    when 5
+    when isMultipleOf5
         puts "Buzz"
     else
         puts num
